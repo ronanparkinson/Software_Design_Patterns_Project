fe804abc3a10616e29a5c7a6b6dd63cb6540401a
t00189412 <Ronan.Parkinson@students.ittralee.ie>
2018-09-19 14:16:47 +0100

    THis commit will add the superclass for products

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    String name = "";
+    float price = 0;
+    String description = "";
+
+    public Product(String description, float price, String name) {
+        this.description = description;
+        this.price = price;
+        this.name = name;
+    }
+}
